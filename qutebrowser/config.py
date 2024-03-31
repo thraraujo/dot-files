6750f442e191ef997193c45d71316f197cce6061
thraraujo <thr.araujo@gmail.com>
2024-03-31 08:46:27 -0300

    improve config

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -154,15 +154,27 @@ config.set('content.local_content_can_access_file_urls', False, 'file:///home/th
 # Dark Mode
 config.set("colors.webpage.darkmode.enabled", True)
 
-## Dracula
-import dracula.draw
+## Dracula: download the repo here: https://draculatheme.com/qutebrowser
+#import dracula.draw
 
 # Load existing settings made via :set
+#config.load_autoconfig()
+
+#dracula.draw.blood(c, {
+#    'spacing': {
+#        'vertical': 6,
+#        'horizontal': 8
+#    }
+#})
+
+
+## Catppuccin
+import catppuccin
+
+# load your autoconfig, use this, if the rest of your config is empty!
 config.load_autoconfig()
 
-dracula.draw.blood(c, {
-    'spacing': {
-        'vertical': 6,
-        'horizontal': 8
-    }
-})
+# set the flavor you'd like to use
+# valid options are 'mocha', 'macchiato', 'frappe', and 'latte'
+# last argument (optional, default is False): enable the plain look for the menu rows
+catppuccin.setup(c, 'mocha', True)
