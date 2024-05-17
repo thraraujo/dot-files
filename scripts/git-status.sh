12cbffe5989266fd5ed0e351d72e7c3fb83fb7e4
thraraujo <thgr.araujo@gmail.com>
2024-05-16 23:33:38 -0300

    modify some definitions

--- scripts/git-status.sh
+++ scripts/git-status.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # These are my git repositories
 gitRepos=(
     $HOME/.config/dot-files/
@@ -14,7 +15,35 @@ gitRepos=(
     $HOME/Sync/projects/work/pySymmPol
 )
 
+
+echo "Here are the options you have:"
+echo "    1. See status."
+
+read -p ">> What option do you want? " CHOICE
+
+echo " "
+
+if [ $CHOICE -eq 1 ] ; then
+
+#echo "Option 1: Go to repo:"
+#for ((i=0; i<${#gitRepos[@]}; i++)); do
+#   echo " $i - ${gitRepos[i]}"
+#done
+#
+#echo " "
+#
+#read -p " What directory do you want? " DIR
+#
+#echo "${gitRepos[$DIR]}"
+#cd "${gitRepos[$DIR]}"
+#
+#else
+
+echo "Option 1: See modified repos"
+echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
 for repo in "${gitRepos[@]}"
 do
-    test -n "$(git -C $repo status --porcelain)" && echo ">> MODIFIED :: $repo" #|| echo "<< NOTHING :: $repo"
+    test -n "$(git -C $repo status --porcelain)" && echo "   >> MODIFIED :: $repo" #|| echo "<< NOTHING :: $repo"
 done
+echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
+fi
