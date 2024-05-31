d017701c06803e7d1505968e07a879995e0c1bc8
thraraujo <thgr.araujo@gmail.com>
2024-05-31 12:48:45 -0300

    add a function to reevaluate the emacs init file

--- emacs/init.el
+++ emacs/init.el
@@ -2,3 +2,13 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+
+;; Some custom functions
+
+(defun eval-init ()
+  "Function to evaluate the init file"
+  (interactive)
+  (setq my-buffer (find-file-noselect "/home/thiago/.config/dot-files/emacs/init.el"))
+  (eval-buffer my-buffer)
+  (message "init file reevaluated"))
+
