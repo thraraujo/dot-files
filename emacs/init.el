84804fb05b53ea38cbd014270d05c50bff9842a7
thraraujo <thgr.araujo@gmail.com>
2024-06-04 18:52:36 -0300

    add functions to my emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -3,16 +3,36 @@
   "config.org"
   user-emacs-directory))
 
-;; Some custom functions
+;; SOME CUSTOM FUNCTIONS
 
-(defun eval-init ()
+(defun th-eval-init ()
   "Function to evaluate the init file."
   (interactive)
   (setq my-buffer (find-file-noselect "~/.config/dot-files/emacs/init.el"))
   (eval-buffer my-buffer)
   (message "init file reevaluated"))
 
-(defun open-config ()
+(defun th-open-config ()
   "Function to open the config file."
   (interactive)
   (find-file "~/.config/dot-files/emacs/config.org"))
+
+(defun th-wiki ()
+  "Function to open the wiki directory."
+  (interactive)
+  (find-file "~/Sync/projects/wiki"))
+
+(defun th-work ()
+  "Function to open the work directory."
+  (interactive)
+  (find-file "~/Sync/projects/work"))
+
+(defun th-dot-files ()
+  "Function to open my dot files"
+  (interactive)
+  (find-file "~/.config/dot-files"))
+
+(defun th-scripts ()
+  "Function to open my dot files"
+  (interactive)
+  (find-file "~/.config/scripts"))
